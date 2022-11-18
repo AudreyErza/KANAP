8fede5e6c3aea9256883105c206a901cea988e63
AudreyErza <audrey.revenault@gmail.com>
2022-11-18 17:31:17 +0100

    Essais page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,19 @@
-let cart = localStorage.getItem(panier);
-console.log(panier)
+// Je récupère les infos du panier après avoir vérifié s'il était plein ou vide" //
+
+let cart = localStorage.getItem('panier');
+    items = [];
+console.log(cart);
+
+if (cart != null){
+    items = JSON.parse(cart);
+}else{
+        console.log(items);
+    }
+
+// Je déclare les champs de la page HTML du panier // 
+let kanapName = document.querySelector("#title"),
+    kanapPrice = document.querySelector("#price"),
+    imgKanap = document.querySelector(".item__img"),
+    descKanap = document.querySelector("#description"),
+    colorKanap = document.querySelector("#colors");
+    kanapQuantity = document.querySelector("#quantity");
