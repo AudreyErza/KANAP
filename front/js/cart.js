efc49688282331c3415ee3dff1d252571e46c89f
AudreyErza <audrey.revenault@gmail.com>
2022-12-02 09:21:55 +0100

    02122022

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,19 +1,72 @@
-// Je récupère les infos du panier après avoir vérifié s'il était plein ou vide" //
+//Je déclare mes variables
+let blocItem = document.querySelector("#cart__items"),
+    boutonOrder = document.querySelector("#order"), //ko//
+    totalPrice = document.querySelector("#totalPrice");
 
-let cart = localStorage.getItem('panier');
+// Je récupèr ge les infos du panier  //
+let panier = localStorage.getItem('panier'),
     items = [];
-console.log(cart);
+console.log(panier);
 
-if (cart != null){
-    items = JSON.parse(cart);
+if (panier!= null){
+    items = JSON.parse(panier);
 }else{
         console.log(items);
+        blocItem.innerHTML += `<h1>est vide<br>:(</h1>`;
     }
+// Je récupère les données de l'API //
+let getKanap = function () {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => response.json ())
+    .then((kanap) => {
+      items.forEach(element => {
+        let product = kanap.find(p => p._id == element.id);
+        blocItem.innerHTML += `<article class="cart__item" data-id="${product._id}" data-color="${element.color}">
+        <div class="cart__item__img">
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+        </div>
+        <div class="cart__item__content">
+          <div class="cart__item__content__description">
+            <h2>${product.name}</h2>
+            <p>${element.color}</p>
+            <p>${product.price}€</p>
+          </div>
+          <div class="cart__item__content__settings">
+            <div class="cart__item__content__settings__quantity">
+              <p>Qté :</p>
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+            </div>
+            <div class="cart__item__content__settings__delete">
+              <p class="deleteItem">Supprimer</p>
+            </div>
+          </div>
+        </div>
+      </article>`
+      });
+    });
+  }
+    getKanap();
+    let boutonSupp = document.querySelectorAll(".deleteItem");
+      console.log(boutonSupp);
+      boutonSupp.forEach( () => {
+        console.log('test');
+      } );
 
-// Je déclare les champs de la page HTML du panier // 
-let kanapName = document.querySelector("#title"),
-    kanapPrice = document.querySelector("#price"),
-    imgKanap = document.querySelector(".item__img"),
-    descKanap = document.querySelector("#description"),
-    colorKanap = document.querySelector("#colors");
-    kanapQuantity = document.querySelector("#quantity");
+// // // Suppression panier //
+//   boutonSupp.addEventListener('click',(kanap)=> {
+//     items.forEach(element => {
+//       if (panier!= null){
+//         items = JSON.parse(panier);
+//         console.log(items.slice(0,4));
+//     }
+    
+//     });
+//   });
+
+
+
+// boutonSupp.addEventListener('click',()=> {console.log('CLIC')});
+//
+
+
+// Change la quantité //
--- front/js/product.js
+++ front/js/product.js
@@ -2,7 +2,15 @@ const urlPage = new URLSearchParams(window.location.search),
       ID= urlPage.get("id")
       url=`http://localhost:3000/api/products/${ID}`
 
-    
+//déclaration des différents champs
+    const kanapName = document.querySelector("#title"),
+        kanapPrice = document.querySelector("#price"),
+        imgKanap = document.querySelector(".item__img"),
+        descKanap = document.querySelector("#description"),
+        colorKanap = document.querySelector("#colors");
+        kanapQuantity = document.querySelector("#quantity");
+        
+        
 // ---------------
 // APPEL DE L'API 
 // ----------------
@@ -13,15 +21,6 @@ fetch(url) // récupère les données contenues dans l'api (ici le catalogue de 
   .then((kanap) => {
     console.table(kanap);
 
-    //déclaration des différents champs
-    let kanapName = document.querySelector("#title"),
-        kanapPrice = document.querySelector("#price"),
-        imgKanap = document.querySelector(".item__img"),
-        descKanap = document.querySelector("#description"),
-        colorKanap = document.querySelector("#colors");
-        kanapQuantity = document.querySelector("#quantity");
-        
-        
     // Affichage des infos dans chaque champ
         kanapName.innerHTML = kanap.name;
         kanapPrice.innerHTML = kanap.price;
@@ -63,6 +62,9 @@ function getPanier() {
 
 // Ajout au panier //
 function addPanier(item){
+  if (kanapQuantity.value<=0 || kanapQuantity.value>100 || colorKanap.value == ''){
+    alert('Merci de selectionner une couleur et de vérifier que la quantité à ajouter est comprise entre 0 et 100.');
+  } else {
   getPanier();
   let panier = getPanier();
   let produitExistant = panier.find(p => p.id == item.id && p.color == item.color);
@@ -70,13 +72,15 @@ function addPanier(item){
     produitExistant.quantity = parseInt(produitExistant.quantity);
     item.quantity = parseInt(item.quantity);
     produitExistant.quantity += item.quantity; 
+    if(produitExistant.quantity>100){
+      alert('La quantité maximale de produit est de 100');
+    }else{
+      savePanier(panier);
+    }
   } else {
     panier.push(item);
+    savePanier(panier);
     }
-  savePanier(panier);
-
-  // Limitation d'article //
-let totalArticle = produitExistant.quantity + item.quantity;
-console.log(totalArticle);
+  }
 }
 
